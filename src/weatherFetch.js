c88c55935e07e0a3308f5ee266a175a6b4b5f44c
Michael <michael.ts.hamilton@gmail.com>
2021-04-01 12:58:28 -0500

    Breaking out project further into modules.

--- src/weatherFetch.js
+++ src/weatherFetch.js
@@ -1,6 +1,8 @@
+import apiToken from './apiToken';
+
 let cityName = 'Milwaukee, WI';
 
-let apiKeyUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f06e2b0078f1ce055f5891af19c4e57`;
+let apiKeyUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiToken}`;
 
 fetch(apiKeyUrl,{mode: 'cors'})
     .then(function(res){
