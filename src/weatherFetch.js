ff897eb1308b5688e5192837abbbbed7f00b47a4
Michael <michael.ts.hamilton@gmail.com>
2021-04-02 21:00:44 -0500

    Continued work on implementing weather API

--- src/weatherFetch.js
+++ src/weatherFetch.js
@@ -1,12 +1,12 @@
 import apiToken from './apiToken';
 
-let cityName = 'Milwaukee, WI';
+let cityName = 'Milwaukee';
 
 let apiKeyUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiToken}`;
 
 fetch(apiKeyUrl,{mode: 'cors'})
     .then(function(res){
-        console.log(res);
+        console.log(res.json()');
     })
     .catch(function(err){
         console.log(err);
